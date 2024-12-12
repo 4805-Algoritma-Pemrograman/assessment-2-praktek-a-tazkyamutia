987daec7de3349b897f510cfda2fc8f753244342
tazkyamutia <tazkyamr34@gmail.com>
2024-12-12 08:39:00 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
+    public static void tampilkanAngkaGanjil(int N) {
+        System.out.println("Angka Ganjil");
+        for (int i = 0; i <= N; i++) {
+        if (i % 2 != 0) {
+            System.out.println(i + "");
+        }
+    }
+    System.out.println();
+}
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan bilangan N: ");
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
+        tampilkanAngkaGanjil(N);
+    
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
