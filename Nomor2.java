334f05e5fe42d64f6f57311f60e82fbbc33ddd73
tazkyamutia <tazkyamr34@gmail.com>
2024-12-12 09:14:30 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,13 +1,56 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
+    static ArrayList<Integer>keranjangBelanja = new ArrayList<>();
+
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+        int[] hargaMenu = {35000, 45000, 20000, 15000};
+        String[] namaMenu = {"Burger", "Steak", "Spaghetti", "Kentang"};
 
-        /* Pekerjaan anda berakhir sini */
+        Scanner scanner = new Scanner(System.in);
+        char inputLagi;
 
-        System.out.println(Nomor2.class.getDeclaredMethods().length); 
+        do {
+            System.out.print("Masukan nama menu [Burger/Steak/Spaghetti/Kentang]:");
+            String inputMenu = scanner.nextLine();
+
+            for (int i = 0; i < namaMenu.length; i++) {
+                if (inputMenu.equalsIgnoreCase(namaMenu[i])) {
+                    keranjangBelanja.add(hargaMenu[i]);
+                    break;
+                }
+            }
+
+            System.out.print("Input lagi (Y/N)?: ");
+            inputLagi = scanner.next().charAt(0);
+            scanner.nextLine(); 
+        } while (inputLagi == 'Y');
+        
+        int totalBelanja = hitungTotalBelanja();
+        int diskon = hitungDiskon(totalBelanja);
+        int totalBayar = totalBelanja - diskon;
+
+        System.out.println("Total item pesanan = " + keranjangBelanja.size());
+        System.out.println("Total yang harus dibayar = " + totalBayar);
+    }
+
+    public static int hitungTotalBelanja() {
+        int total = 0;
+        for (int harga : keranjangBelanja) {
+            total += harga;
+        }
+        return total;
+    }
+
+    public static int hitungDiskon(int totalBelanja) {
+        if (totalBelanja < 50000) {
+            return 0;
+        } else if (totalBelanja >= 50000 && totalBelanja <= 100000) {
+            return (int) (totalBelanja * 0.05);
+        } else {
+            return (int) (totalBelanja * 0.1);
+        } 
     }
 }
+
